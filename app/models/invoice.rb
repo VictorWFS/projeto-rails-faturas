a7729ebebe11e378c31507c9fc4721ebe3007aa2
VictorWFS <victorwfs900@gmail.com>
2025-11-13 12:52:20 -0300

    feat: calcula total da fatura

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :line_items, dependent: :destroy
+
+  def calculated_total
+    line_items.sum { |item| item.quantity * item.unit_price}
+  end
+  
 end
